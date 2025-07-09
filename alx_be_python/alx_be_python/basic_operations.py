14b3a0da7b2a0473cc8b74cb838a659de4381b21
Midehayzed <midehayzed@gmail.com>
2025-07-09 14:24:31 +0100

    Fix folder structure and track files

--- alx_be_python/alx_be_python/basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-sum = number1 + number2
-difference = number1 - number2
-product = number1 * number2
-print("Sum:", sum)
-print("Difference:", difference)
-print("Product:", product)
